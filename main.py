d7819d2c8ae8c5170da21c04a95a031d72a5ae3d
M-YOUSUF-M <yousufmazumder12@gmail.com>
2025-02-09 14:02:00 +0600

    update main.py

--- main.py
+++ main.py
@@ -18,9 +18,8 @@ class MainWindow(QWidget):
         # Dropdown menu
         self.main_drop_down_box = QComboBox()
         self.main_drop_down_box.setObjectName("drop_down")
-        self.main_drop_down_box.addItem("LIBRARY")
         self.main_drop_down_box.addItem("IDE")
-        self.main_drop_down_box.addItem("DASHBOARD")
+        self.main_drop_down_box.addItem("LIBRARY")
         self.main_drop_down_box.activated.connect(self.changeOptions)
 
         # Widgets for different sections
@@ -29,8 +28,8 @@ class MainWindow(QWidget):
 
         # QStackedWidget to manage switching between widgets
         self.stacked_widget = QStackedWidget()
-        self.stacked_widget.addWidget(self.library_widget)
         self.stacked_widget.addWidget(self.editor_widget)
+        self.stacked_widget.addWidget(self.library_widget)
 
         # Layout for dropdown and stacked widget
         self.selection_layout = QVBoxLayout()
@@ -45,7 +44,4 @@ class MainWindow(QWidget):
         # Switch to the corresponding widget in QStackedWidget
         current_index = self.main_drop_down_box.currentIndex()
         self.stacked_widget.setCurrentIndex(current_index)
-        if current_index == 0:
-            print('library.py is selected')
-        elif current_index == 1:
-            print('editor.py is selected')
+
